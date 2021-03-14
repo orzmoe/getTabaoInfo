1285b3e45bada8ebd98a01787c6c8f23c924de14
lalala <249508908>
2021-03-14 16:30:34 +0800

    1

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func getHtml(_url string, isMobile bool) (string, error) {
 func GetBabyInfo(id, babyType string) (BabyInfoResp, error) {
 	babyInfoResp := BabyInfoResp{}
 	if babyType == "1" || babyType == "0" {
-		_url := "https://detail.m.tmall.com/item.htm?id=" + id
+		_url := "https://m.intl.taobao.com/detail/detail.html?id=" + id
 		html, err := getHtml(_url, true)
 		if err != nil {
 			return babyInfoResp, err
